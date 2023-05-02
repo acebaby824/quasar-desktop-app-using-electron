e2122017a791cea8a10f67be77f9c3b4c939b6d0
babyforever2022 <makpushkin1224@gmail.com>
2023-05-02 16:38:44 +0900

    index page create

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,239 @@
+<template>
+  <div class=" pages">
+  <q-page class="column page-content">
+    <q-dialog v-model="inception" >
+      <q-card class="card-add">
+        <div class="modal_inner">
+          <q-card-section style="padding-left: 0px;">
+            <div class="text-h6" ><q-icon name="add_circle_outline"  class="cross-icon"/>Add Misify</div>
+          </q-card-section>
+          <div>
+            <div class="modal-what">
+              <div v-if="nexton==1" >
+                <q-card-section class="q-pt-none">
+                  Name
+                  <q-input filled v-model="text" label="Filled" />
+                </q-card-section>
+                <q-card-section class="q-pt-none">
+                  Limit
+                  <div class="rows"><q-input filled v-model="text" class="limit-in" label="Filled" /><q-select class="limit-sel" filled v-model="model" :options="modaloptions" label="Filled" /></div>
+              </q-card-section>
+              </div>
+              <div v-else-if="nexton==2">
+                <q-card-section class="q-pt-none">
+                  Type
+                  <!-- <q-input filled v-model="text" label="Filled" class="doneinput"/> -->
+                  <q-select class="doneinput" filled v-model="model" :options="modaloptions" label="Filled" />
+                </q-card-section>
+                <q-card-section class="q-pt-none">
+                  API UserName
+                  <q-input filled v-model="text" label="Filled" class="doneinput"/>
+                </q-card-section>
+                <q-card-section class="q-pt-none">
+                  API Password
+                  <q-input filled v-model="text" label="Filled" class="doneinput"/>
+                </q-card-section>
+                <q-card-section class="q-pt-none">
+                  Currency
+                  <!-- <q-input filled v-model="text" label="Filled" class="doneinput"/> -->
+                  <q-select class="doneinput" filled v-model="model" :options="modaloptions" label="Filled" />
+                </q-card-section>
+              </div>
+            </div>
+
+            <q-card-actions align="right" v-if="nexton==1">
+              <q-btn label="Next"  @click="secondDialog" class="card-next"/>
+            </q-card-actions>
+            <q-card-actions align="right" class="bg-white text-teal" v-else-if="nexton==2">
+              <q-btn ref="Dialog"  @click="secondDialog" class="card-next" label="Done" />
+            </q-card-actions>
+          </div>
+        </div>
+      </q-card>
+    </q-dialog>
+    <div class="row justify-between" style="border-bottom: solid 1px #F3F3F5;">
+      <div  class="searchpage">
+        <input type="search" class="search" placeholder="Search" style="border:none;padding-left: 20px;">
+        <q-icon name="search" style="position: relative; right: 50px;"/>
+      </div>
+      <div class="row">
+        <q-btn round no-outline icon="mdi-bell-outline" class="bell-notification" >
+          <q-badge floating color="#4EFDEA" rounded style="position: absolute;right: 13px;top:16px">4</q-badge>
+        </q-btn>
+        <select  class="brand">
+          <option>
+            Brand Shop
+          </option>
+          <option>
+            Facebook
+          </option>
+          <option>
+            Twitter
+          </option>
+          <option>
+            Oracle
+          </option>
+        </select>
+      </div>
+    </div>
+    <div class="column">
+      <div class="row justify-between">
+        <h5 class="page-name" >Midsify</h5>
+        <div class="mytooltip">
+          <div style="">Reserve 160 Days <span style="position: relative;left: 45%;">50K</span></div>
+          <div style="width: 20%;"><q-linear-progress class="sumupprogress" size="25px" :value="sumupprogress" /></div>
+          <div class="mytext"><div class="sumup-text-up">Release estimate <p class="sumup-text-down">March 4, 2023</p></div></div>
+        </div>
+        <div>
+          <q-btn color="blue" @click="inception=true" icon="add_circle_outline" label="Add"  class="addmidsify"/>
+        </div>
+      </div>
+      <div class="row">
+        <q-table
+          class="my-sticky-virtscroll-table w-100"
+          row-key="name"
+          :rows="rows"
+          :columns="columns"
+          hide-bottom
+        >
+          <template v-slot:body="props">
+            <q-tr :props="props" class="tr-pro">
+              <q-td key="name" align="center" :props="props" style="width:20%">
+                <div class="highlight">{{ props.row.name }}</div>
+              </q-td>
+              <q-td key="progress" :props="props" style="width:50%;padding-left: 25px;" >
+
+                  <div v-if="props.row.status%2==1">
+                    <div class="tooltip">
+                      <q-linear-progress size="25px" :value= props.row.progress  color="blue" class="progress">
+                        <div class="absolute-full flex flex-center">
+                          <q-badge style="height: 200%;  position:absolute; left:52%;" class="badge-bg" :style="{marginLeft:props.row.progress*100-55+'%'}" color="white" text-color="accent" :label="progressLabel1"/>
+                        </div>
+                      </q-linear-progress>
+                      <p class="tooltiptext"  :style="{marginLeft:props.row.progress*50-15+'%'}">{{props.row.uptext}}<br><span class="downspan">{{props.row.downtext}}</span></p>
+                    </div>
+                  </div>
+                  <div v-if="props.row.status%2==0">
+                    <div class="tooltip">
+                      <q-linear-progress  size="25px" :value= props.row.progress  color="#4EFDEA" class="progress1">
+                        <div class="absolute-full flex flex-center">
+                          <q-badge style="height: 200%;  position:absolute; left:52%" :style="{marginLeft:props.row.progress*100-55+'%'}" color="white" text-color="accent" :label="progressLabel1"/>
+                        </div>
+                      </q-linear-progress>
+                      <p class="tooltiptext"  :style="{marginLeft:props.row.progress*50-15+'%'}">{{props.row.uptext}}<br><span class="downspan">{{props.row.downtext}}</span></p>
+                    </div>
+                  </div>
+
+                <div class="my-table-details">
+                  {{ props.row.details }}
+                </div>
+              </q-td>
+              <q-td key="reserve" :props="props" class="reserve" style="width:20%;padding-left: 25px;">
+                {{ props.row.reserve }}
+              </q-td>
+              <q-td key="status" :props="props">
+                <div v-if="props.row.status==1">
+                  <div class="Live" >
+                    <i class="q-icon notranslate material-icons Liveicon" aria-hidden="true" role="presentation" style="font-size: 5px;color:red">circle-small</i>Live
+                  </div>
+                </div>
+                <div v-if="props.row.status==2">
+                  <div class="Next" >
+                    <i class="q-icon notranslate material-icons Liveicon" aria-hidden="true" role="presentation" style="font-size: 5px;color:blue">circle-small</i>Next
+                  </div>
+                </div>
+              </q-td>
+            </q-tr>
+          </template>
+        </q-table>
+      </div>
+    </div>
+  </q-page>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue'
+const columns = [
+  { name: 'progress', align: 'left', label: 'Name', field: 'name', sortable: true },
+  { name: 'name', required: true, label: 'Progress', align: 'left', field: row => row.progress, format: val => `${val}`, sortable: true },
+  { name: 'status', align: 'left', label: 'Reserve', field: 'status', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+  { name: 'reserve', align: 'left', label: 'Status', field: 'reserve', sortable: true }
+]
+
+const rows = [
+  {
+    name: 'Maverick',
+    progress: 0.7,
+    reserve: '$25k',
+    downtext: '$3000',
+    uptext: 'Reserve',
+    status: 1
+  },
+  {
+    name: 'CMS',
+    progress: 0.4,
+    reserve: '$100k',
+    downtext: '10k/100k',
+    uptext: 'Earning',
+    status: 2
+  },
+  {
+    name: 'CMS',
+    progress: 0.5,
+    reserve: 'For 5 days',
+    downtext: '2/5',
+    uptext: 'Days',
+    status: 3
+  },
+  {
+    name: 'Maverick',
+    progress: 0.6,
+    reserve: '15k per Transaction',
+    downtext: '2k/15k',
+    uptext: 'Toatal Transaction',
+    status: 4
+  }
+]
+
+export default {
+
+  setup () {
+    // const subpropos= ref("33%")
+    const progress1 = ref(0.1)
+    const sumupprogress = ref(0.8)
+    return {
+      columns,
+      inception: ref(false),
+      // secondDialog: ref(false),
+      rows,
+      model: ref(null),
+      sumupprogress,
+      // subpropos,
+      progress1,
+      progressLabel1: computed(() => (progress1.value * 100) + '%'),
+      options: [
+        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
+      ],
+      modaloptions: [
+        'Days', 'Transaction', 'Volume'
+      ]
+    }
+  },
+  data () {
+    return {
+      nexton: 1
+    }
+  },
+  methods: {
+    secondDialog: function (event) {
+      if (this.nexton === 0) this.nexton = 1
+      else if (this.nexton === 1) this.nexton = 2
+      else if (this.nexton === 2) { this.nexton = 1; this.inception = false }
+      // console.log(this.nexton)
+    }
+  }
+
+}
+
+</script>
